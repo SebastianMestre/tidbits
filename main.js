575d2947a1470016e87e7bac03fb18caf0e5d06b
SebastianMestre <mestresebastianunr@gmail.com>
2020-02-01 16:57:27 -0300

    re-render only the parts that change. Still needs some work

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let ctx = canvas.getContext("2d");
 let widthInPixels = canvas.width;
 let heightInPixels = canvas.height;
 
-let widthInCells = 20;
+let widthInCells  = 20;
 let heightInCells = 15;
 let totalCells = widthInCells * heightInCells;
 
@@ -25,53 +25,71 @@ let active = new Array(totalCells);
 active.fill(false);
 active = active.map(() => Math.random() < 0.5);
 
-// Would it be nicer if, instead of drawing them as we go, we returned an array
-// of line segments and drew them all at once?
-//
-// Maybe. TODO?
-
 function index (x, y) {
 	return y * widthInCells + x;
 }
 
-const lines = [
-	[],     // 0000
-	[0, 1], // 0001
-	[1, 2], // 0010
-	[0, 2], // 0011
-	[3, 0], // 0100
-	[1, 3], // 0101
-	[1, 2, 3, 0], // 0110
-	[2, 3], // 0111
-	[2, 3], // 1000
-	[0, 1, 2 , 3], // 1001
-	[1, 3], // 1010
-	[3, 0], // 1011
-	[0, 2], // 1100
-	[1, 2], // 1101
-	[0, 1], // 1110
-	[], // 1111
-];
-function drawLines (x, y, l) {
+function marchingSquares (startX, startY, endX, endY) {
+	const LUT = [
+		[],     // 0000
+		[0, 1], // 0001
+		[1, 2], // 0010
+		[0, 2], // 0011
+		[3, 0], // 0100
+		[1, 3], // 0101
+		[1, 2, 3, 0], // 0110
+		[2, 3], // 0111
+		[2, 3], // 1000
+		[0, 1, 2 , 3], // 1001
+		[1, 3], // 1010
+		[3, 0], // 1011
+		[0, 2], // 1100
+		[1, 2], // 1101
+		[0, 1], // 1110
+		[], // 1111
+	];
+
 	const pos = [
 		[ 1,  0],
 		[ 0,  1],
 		[-1,  0],
 		[ 0, -1],
 	];
-	let w = spacingWidth / 2;
-	let h = spacingHeight / 2;
+	const w = spacingWidth / 2;
+	const h = spacingHeight / 2;
 
-	for(let i = 0; i < l.length; i += 2){
-		const p0 = l[i];
-		const p1 = l[i+1];
+	const result = [];
+	for (let cellY = startY+1; cellY < endY; ++cellY){
+		for (let cellX = startX+1; cellX < endX; ++cellX){
+			let a0 = Number(active[index(cellX  , cellY  )]);
+			let a1 = Number(active[index(cellX-1, cellY  )]);
+			let a2 = Number(active[index(cellX  , cellY-1)]);
+			let a3 = Number(active[index(cellX-1, cellY-1)]);
+			let conf = a0 << 0 | a1 << 1 | a2 << 2 | a3 << 3; 
 
-		ctx.moveTo(x + pos[p0][0] * w, y + pos[p0][1] * h);
-		ctx.lineTo(x + pos[p1][0] * w, y + pos[p1][1] * h);
+			const x = cellX * spacingWidth;
+			const y = cellY * spacingHeight;
+
+			const l = LUT[conf];
+			for(let i = 0; i < l.length; i += 2){
+				const p0 = l[i];
+				const p1 = l[i+1];
+
+				let x0 = x + pos[p0][0] * w;
+				let y0 = y + pos[p0][1] * h;
+				let x1 = x + pos[p1][0] * w;
+				let y1 = y + pos[p1][1] * h;
+
+				result.push(x0, y0, x1, y1);
+			}
+		}
 	}
+
+	return result;
 }
 
-function draw () {
+
+function fullDraw () {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 	ctx.fillStyle = "#2c3";
@@ -94,22 +112,54 @@ function draw () {
 		}
 	}
 
+	drawLines(0, 0, widthInCells, heightInCells);
+}
+
+function updatedDraw (cellX, cellY) {
+
+	{
+		const x = cellX * spacingWidth + offsetWidth;
+		const y = cellY * spacingHeight + offsetHeight;
+
+		ctx.clearRect(
+			x - spacingWidth,            y - spacingHeight,
+			blockWidth + 2*spacingWidth, blockHeight + 2*spacingHeight
+		);
+	}
+
+	for (let dx = -1; dx <= 1; ++dx) {
+		for (let dy = -1; dy <= 1; ++dy) {
+
+			let cx = cellX + dx;
+			let cy = cellY + dy;
+
+			if (active[index(cx, cy)]) {
+				ctx.fillStyle = "#2c3";
+			} else {
+				ctx.fillStyle = "#c23";
+			}
+
+			const x = cx * spacingWidth + offsetWidth;
+			const y = cy * spacingHeight + offsetHeight;
+
+			ctx.fillRect(x, y, blockWidth, blockHeight);
+		}
+	}
+
+	drawLines(
+		Math.max(0, cellX-2), Math.max(0, cellY-2),
+		Math.min(widthInCells, cellX+4), Math.min(heightInCells, cellY+4)
+	);
+}
+
+function drawLines (startX, startY, endX, endY) {
+	const lines = marchingSquares(startX, startY, endX, endY);
 	ctx.lineWidth = 2;
 	ctx.strokeStyle = "#ccd";
 	ctx.beginPath();
-	for (let cellY = 1; cellY < heightInCells; ++cellY){
-		for (let cellX = 1; cellX < widthInCells; ++cellX){
-			let a0 = Number(active[index(cellX  , cellY  )]);
-			let a1 = Number(active[index(cellX-1, cellY  )]);
-			let a2 = Number(active[index(cellX  , cellY-1)]);
-			let a3 = Number(active[index(cellX-1, cellY-1)]);
-			let conf = a0 << 0 | a1 << 1 | a2 << 2 | a3 << 3; 
-
-			const x = cellX * spacingWidth;
-			const y = cellY * spacingHeight;
-
-			drawLines(x, y, lines[conf]);
-		}
+	for (let i = 0; i < lines.length; i += 4) {
+		ctx.moveTo(lines[i+0], lines[i+1]);
+		ctx.lineTo(lines[i+2], lines[i+3]);
 	}
 	ctx.stroke();
 }
@@ -133,7 +183,7 @@ canvas.addEventListener("click", function(evt){
 
 	active[cellIndex] = !active[cellIndex];
 
-	draw();
+	updatedDraw(cellX, cellY);
 });
 
-draw();
+fullDraw();
