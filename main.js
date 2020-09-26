ff248dc54d5654e79512ab9e4a5d4124e643034f
SebastianMestre <mestresebastianunr@gmail.com>
2020-09-26 09:18:44 -0300

    limit update to twice per second

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const share = document.getElementById('ShareLink');
 const input = document.getElementById('MathInput');
 const output = document.getElementById('MathPreview');
+
+let need_update = true;
+
 const update = function(){
+	if (!need_update)
+		return;
+	need_update = false;
+
 	const content = input.value.trim();
 	const encodedContent = encodeURI(content);
 	output.textContent = `$$${input.value.trim()}$$`;
@@ -11,10 +18,16 @@ const update = function(){
 	MathJax.typesetClear();
 	MathJax.typesetPromise([output]);
 };
-input.addEventListener("input", update);
+
+input.addEventListener("input", function(){
+	need_update = true;
+});
+
 window.addEventListener("load", function(){
 	const content = window.location.hash.substr(1);
 	if (content.length > 0)
 		input.value = decodeURIComponent(content);
-	update();
+	need_update = true;
+
+	window.setInterval(update, 500);
 });
