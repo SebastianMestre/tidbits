5c28d56418d7cdea1ae27bb56a8caf62bed5be31
SebastianMestre <mestresebastianunr@gmail.com>
2020-10-21 20:48:20 -0300

    draw the instructions as they execute

--- main.js
+++ main.js
@@ -12,6 +12,42 @@ const drawArray = (array, x0, y0, xs, ys) => {
 	}
 };
 
+const drawInstructions = (program, pc, x0, y0, ys) => {
+	const radius = 3;
+	const textHeight = 20;
+	const lineSpacing = 4;
+	const lineHeight = textHeight + lineSpacing;
+
+	ctx.font = `${textHeight}px monospace`;
+
+	const show = [];
+	for(let idx = pc - radius; idx <= pc + radius; ++idx){
+		if(idx < 0 || idx >= program.length) show.push({});
+		else show.push(program[idx]);
+	}
+
+	let ypos = y0;
+	for(let i = 0; i < show.length; ++i){
+		const line = JSON.stringify(show[i]);
+		if (i != radius)
+			ctx.fillText(line, x0, ypos + lineHeight);
+		ypos += lineHeight;
+	}
+
+	ctx.fillStyle = "#fff";
+	ctx.fillText(JSON.stringify(show[radius]), x0, y0 + (radius + 1) * lineHeight);
+};
+
+const draw = (machine, program) => {
+	ctx.fillStyle = "#808";
+	ctx.fillRect(0, 0, 800, 600);
+
+	ctx.fillStyle = "#f0f";
+	drawArray(machine.memory, 0, 0, 800, 600);
+
+	ctx.fillStyle = "#000";
+	drawInstructions(program, machine.state["PC"], 20, 20, 600);
+};
 
 
 
@@ -58,22 +94,14 @@ const shuffle = (array) => {
 	return array;
 }
 
-const N = 100;
+const N = 10;
 const machine = {
 	state: {
 		A: 0, B: 0, C: 0, D: 0, E: 0, F: 0,
 		PC: 0,
 	},
-	memory: shuffle([...new Array(N)].map((x,i,a) => 1.05-(i+1)/a.length)),
+	memory: shuffle([...new Array(N)].map((x,i,a) => (i+1)/a.length)),
 	stack: [],
-}
-
-const draw = () => {
-	ctx.fillStyle = "#808";
-	ctx.fillRect(0, 0, 800, 600);
-
-	ctx.fillStyle = "#f0f";
-	drawArray(machine.memory, 0, 0, 800, 600);
 }
 
 // bubble sort
@@ -107,5 +135,5 @@ const id = setInterval(() => {
 	}
 	const op = program[machine.state["PC"]];
 	step(machine, program);
-	draw();
-}, 0);
+	draw(machine, program);
+}, 100);
