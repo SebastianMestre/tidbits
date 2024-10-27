fcfda271612811180a386cf4cdae6ae38cec87ae
Sebastian Mestre <mestresebastianunr@gmail.com>
2024-10-27 20:24:58 -0300

    random tweaks

--- radiance-cascades/main.js
+++ radiance-cascades/main.js
@@ -12,7 +12,7 @@ function init(w, h) {
 
 	const start = performance.now();
 
-	const base_dist = 1.5 / 4 ** cascade_count;
+	const base_dist = 1.5 / 3 ** cascade_count;
 	console.log("base_dist", base_dist);
 
 	for (let level = cascade_count-1; level >= 0; --level) {
@@ -32,8 +32,8 @@ function init(w, h) {
 			dy.push(Math.sin(2 * Math.PI / ray_count * (i + 0.5)));
 		}
 
-		const start_dist = level == 0 ? 0 : base_dist * 4 **  level;
-		const end_dist   =                  base_dist * 4 ** (level + 1);
+		const start_dist = level == 0 ? 0 : base_dist * 3 **  level;
+		const end_dist   =                  base_dist * 3 ** (level + 1);
 
 		for (let i = 0; i < ch; ++i) {
 			for (let j = 0; j < cw; ++j) {
@@ -67,18 +67,19 @@ function init(w, h) {
 
 				const probe_idx_premul = (i * cw + j) * ray_count;
 
-				// image-space position
+				// image-space position of the current probe
 				const x =     (j + 0.5) * probe_size / w;
 				const y = 1 - (i + 0.5) * probe_size / h;
 
-				// top-left sample in next cascade
+				// top-left probe in next cascade
 				const nj = ((j + 1) >> 1) - 1;
 				const ni = ((i + 1) >> 1) - 1;
 
+				// image space position of probe in the next cascade
 				const nx =     (nj + 0.5) * nprobe_size / w;
 				const ny = 1 - (ni + 0.5) * nprobe_size / h;
 
-				// sample-space distance to top-left sample
+				// sample-space distance to top-left probe
 				const ex = (x - nx) * w / nprobe_size;
 				const ey = (ny - y) * h / nprobe_size;
 
@@ -116,20 +117,10 @@ function init(w, h) {
 	console.log("precomputed radiance cascade in", ending - start, "milliseconds");
 }
 
-function noise(x, y) {
-	return Math.sin(x-1) + Math.cos(x - 3*y) - Math.sin(2*x + y);
-}
-
-function is_wall(x, y) {
-	return noise(x * 10, y * 10) < -0.9;
-}
-
 // returns radiance in [r,g,b] 0..1 format
-// it does this by raymarching the is_wall function
+// it does this by ball-tracing a lipschitz function
 function trace(x, y, dx, dy, start_distance, end_distance) {
 	let c = [0,0,0,0,0];
-
-	const stride = 0.0016;
 
 	let hit = false;
 	let walked = start_distance;
@@ -141,14 +132,23 @@ function trace(x, y, dx, dy, start_distance, end_distance) {
 		if (xx > 1) { hit = true; side = 1; break; }
 		if (yy < 0) { hit = true; side = 2; break; }
 		if (yy > 1) { hit = true; side = 3; break; }
-		if (is_wall(xx, yy)) { hit = true; side = 4; break; }
-		walked += stride;
+
+		const d = sdf(xx, yy);
+		if (d < 0.001) { hit = true; side = 4; break; }
+
+		walked += d;
 		if (walked > end_distance) { side = 4; break; }
 	}
 
 	c[side] += 1;
 
 	return [c[0]+c[3], c[1]+c[3], c[2]+c[3], Number(!hit)];
+
+	function sdf(x, y) {
+		return Math.min(
+			(Math.sin(10*x-1) + Math.cos(10*x - 30*y) - Math.sin(20*x + 10*y) + 0.9) / 80,
+			Math.min(1.1 - Math.abs(x), 1.1 - Math.abs(y)));
+	}
 }
 
 function color(x, y) {
